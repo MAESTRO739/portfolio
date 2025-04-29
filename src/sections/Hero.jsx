1ba97568290f5f4082d0ce8ecca6f518483013ef
MAESTRO739 <butakova.tatyana79@gmail.com>
2025-04-28 22:14:01 -0400

    title animation, scroll and counter

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,26 @@
 import { words } from "../../constants/index.js";
 import Button from "../components/Button.jsx";
+import HeroExperience from "../components/HeroModels/HeroExperience.jsx";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import AnimatedCounter from "../components/AnimatedCounter.jsx";
 
 const Hero = () => {
+    useGSAP(() => {
+        gsap.fromTo('.hero-text h1',
+            {
+                y: 50,
+                opacity: 0,
+            },
+            {
+                y: 0,
+                opacity: 1,
+                stagger: 0.2,
+                duration: 1,
+                ease: 'power2.inOut',
+            })
+    })
+
     return (
         <section id='hero' className='relative overflow-hidden'>
             <div className='absolute top-0 w-full h-full'>
@@ -9,7 +28,7 @@ const Hero = () => {
             </div>
 
             <div className='hero-layout'>
-                <header className='flex flex-col justify-center md:w-full w-screen m:px-20 px-5'>
+                <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
                     <div className='flex flex-col gap-7'>
                         <div className='hero-text'>
                             <h1>
@@ -45,7 +64,14 @@ const Hero = () => {
                         />
                     </div>
                 </header>
+
+                <figure>
+                    <div className='hero-3d-layout'>
+                        <HeroExperience />
+                    </div>
+                </figure>
             </div>
+            <AnimatedCounter />
         </section>
     )
 }
